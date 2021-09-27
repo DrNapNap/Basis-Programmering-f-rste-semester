b1203d0556a8c686573f27c7aca88cd5be005eaf
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2021-09-27 19:02:57 +0200

    hey

--- Basis Programmering, første semester/Program.cs
+++ Basis Programmering, første semester/Program.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Basis_Programmering__første_semester
 {
     class Program
     {
 
+        
+
 
         static void Main(string[] args)
         {
 
-
+            
             Start();
 
 
 
-            
+
 
 
 
@@ -27,19 +25,16 @@ namespace Basis_Programmering__første_semester
         }
 
 
-     
-           static void Loopd()
+
+        static void Loopd()
         {
             int[,] myArrayd = new int[1, 5];
 
-            string[] myArray = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
 
-            string arrayStrig = myArray[4];
-            Console.WriteLine(arrayStrig);
 
             for (int y = 0; y < 1; y++)
             {
-                Console.WriteLine("     ___________________________________________________________________________   ");
+                Console.WriteLine("        ___________________________________________________________________________________   ");
                 Console.WriteLine("");
                 for (int x = 0; x < 5; x++)
                 {
@@ -48,7 +43,7 @@ namespace Basis_Programmering__første_semester
                     {
                         Console.Write("");
                     }
-                    Console.Write("        " + myArrayd.GetLength(myArrayd[y, x]) + "     |");
+                    Console.Write("       |   " + myArrayd.GetLength(myArrayd[y, x]) + "     |");
                 }
 
                 Console.WriteLine("");
@@ -61,7 +56,7 @@ namespace Basis_Programmering__første_semester
 
 
 
-    static void Start()
+        static void Start()
         {
             Console.WriteLine();
             Console.WriteLine("Velkommen til Jeopardy");
@@ -95,11 +90,6 @@ namespace Basis_Programmering__første_semester
 
 
 
-
-            int[,] myArray = new int[5, 5];
-
-           
-
             Loopd();
             for (int y = 0; y < 5; y++)
             {
@@ -110,9 +100,9 @@ namespace Basis_Programmering__første_semester
 
                     if (x == 0)
                     {
-                        
 
-                        Console.Write("| " + (200 * y + 100  ) + " |");
+
+                        Console.Write("| " + (200 * y + 100) + " |");
                     }
                     Console.Write("      " + (200 * y + 100) + "       |");
                 }
@@ -131,63 +121,134 @@ namespace Basis_Programmering__første_semester
 
 
 
-    static string Categoryselector()
+        static string Categoryselector()
         {
 
+            string[] myArray = new string[] { " Skriv kategorienavn", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
 
-            string text = "Skive kategorienavn";
+            string arrayStrig = myArray[0];
+
+            
+            int[] myArrayd = new int[] { 100, 300, 500, 700, 900 };
+            int pointSystem1 = myArrayd[0];
+            int pointSystem2 = myArrayd[1];
+            int pointSystem3 = myArrayd[2];
+            int pointSystem4 = myArrayd[3];
+            int pointSystem5 = myArrayd[4];
+            
+
+
+            string text = arrayStrig;
 
             string tolo = text.ToUpper();
 
             Console.WriteLine();
             Console.WriteLine(tolo);
 
-            
+bool tenker = true;
 
             string selector = "";
- bool restaet = false;
+            bool restaet = false;
             do
             {
                 restaet = false;
 
- 
+                string[] dwa = new string[] { " Generel viden", "Geografi", "Sprog, litteratur og kunst:", "Historie og mode", "Popkultur og musik" };
 
                 selector = (Console.ReadLine());
-
-                if (selector == "1")
+                
+                if (selector == "Generel viden")
                 {
+                    int selectorSvorhedsGrad;
+                    Console.WriteLine("Skriv Hvilken sværhedsgrad");
+                    selectorSvorhedsGrad = int.Parse( (Console.ReadLine()));
+
+                                                if (tenker == false)
+                            {
+                                Console.WriteLine("du har været her før du prøver at snyde");
+                            }
+
+                  if(tenker == true)
+                    { 
+                    if (selectorSvorhedsGrad == 100 )
+                    {
+                        Console.WriteLine("Hvor mange tidszoner er der I Rusland?");
+                        Console.WriteLine("");
+                        Console.WriteLine("Skriv dit svar her");
+                        string rusLand;
+
+                            rusLand = (Console.ReadLine());
 
 
-                    Console.WriteLine("3");
+
+                            if (rusLand == "11")
+                        {
+
+                            Console.WriteLine("Tillykke du har vundet 100 point");
+                                tenker = false;
+
+
+                            }
+                            else
+                        {
+                            Console.WriteLine("Svaret er ikke korrekt");
+                            
+                        }
+ }}
+                    
+                    if (selectorSvorhedsGrad == 300)
+                    {
+                        Console.WriteLine("dwawd");
+                    }
+                    if (selectorSvorhedsGrad == 500)
+                    {
+
+                    }
+                    if (selectorSvorhedsGrad == 700)
+                    {
+
+                    }else if (selectorSvorhedsGrad == 900) {
+
+                    }else
+                    {
+                        restaet = true;
+                    }
+
                 }
-                else if (selector == "2")
+                else if (selector == "Geografi")
                 {
                     Console.WriteLine("test");
                 }
                 else
                 {
                     restaet = true;
-                    
-                    if (restaet == true )
+
+                    if (restaet == true)
                     {
-                      Console.WriteLine("Der er en fail");
+                        Console.WriteLine("Der er en fail");
                         Console.WriteLine("skive i igen");
                     }
                 }
- 
-                
+
+
             } while (restaet);
-            Console.WriteLine("dwa");
-return selector;
+        
+            return selector;
         }
 
 
+        //static string tberegner()
+        //{
 
-  
 
-    static void h1()
+            
+        //}
+
+
+
+        static void h1()
         {
-          
+
             string s = "==============" + " Jeopardy " + "==============";
 
 
@@ -196,14 +257,13 @@ return selector;
 
             Console.SetCursorPosition((Console.WindowWidth - toUpper.Length) / 4, Console.CursorTop + 2);
 
-           
 
-            
+
+
             Console.WriteLine(toUpper);
-       
+
         }
 
 
-
-}
+    }
 }
